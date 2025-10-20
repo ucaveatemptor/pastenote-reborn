2bde8b779663d95ed63e776d32bf2160521b4915
ucaveatemptor <normagaok@gmail.com>
2025-10-21 01:48:25 +0300

    init backend

---
+++ backend/internal/database/db.go
@@ -0,0 +1,41 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+var DB *sql.DB
+
+func InitDB() (*sql.DB, error) {
+	err := godotenv.Load("../.env")
+	if err != nil {
+		log.Print("Error loading .env file:", err)
+		return nil, err
+	}
+
+	host := os.Getenv("POSTGRES_HOST")
+	port := os.Getenv("POSTGRES_PORT")
+	user := os.Getenv("POSTGRES_USER")
+	password := os.Getenv("POSTGRES_PASSWORD")
+	dbname := os.Getenv("POSTGRES_DB")
+
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := db.Ping(); err != nil {
+		log.Print("Failed to connect to the database:", err)
+		return nil, err
+	}
+	log.Println("Successfully connected to psqlDB")
+	return db, nil
+}
